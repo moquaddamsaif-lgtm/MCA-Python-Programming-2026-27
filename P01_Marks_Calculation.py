0d51c5aa7b40f08a18dd26d15e691aa15e7654c1
Moquaddam <moquaaddamsaif@gmail.com>
2026-09-12 18:13:02 +0530

    Added P01 Marks Calculation program

---
+++ P01_Marks_Calculation.py
@@ -0,0 +1,16 @@
+sub_1 = int(input("Enter marks of sub_1: "))
+sub_2 = int(input("Enter marks of sub_2: "))
+sub_3 = int(input("Enter marks of sub_3: "))
+sub_4 = int(input("Enter marks of sub_4: "))
+sub_5 = int(input("Enter marks of sub_5: "))
+
+total_marks = sub_1 + sub_2 + sub_3 + sub_4 + sub_5
+print("Total Marks:", total_marks)
+
+
+average_marks = total_marks / 5
+print("Average Marks:", average_marks)
+
+percentage = (total_marks / 500) * 100
+print("Percentage:", percentage)
+
